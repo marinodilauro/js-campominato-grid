4d80fb44b7d97cba73cd0048566a8b2ca778e78c
marinodilauro <m.dilauro90@gmail.com>
2024-02-28 19:30:41 +0100

    Add a reset button

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,10 @@
 
 const playBtnElem = document.getElementById("play_btn");
 
+// Create a variable for the "Reset" button DOM element
+
+const resetBtnElem = document.getElementById("reset_btn");
+
 // Create a variable for the header DOM element
 
 const headerElem = document.getElementById("site_header");
@@ -19,13 +23,13 @@ let cellsNumber = 100;
 
 // Create a variable for the single grid element markup
 
-let cellMarkup = `<div class="cell easy"></div>`;
+let cellMarkup = `<div class="cell"></div>`;
 
 
 // Create a variable to prevent generation of multiple grids after the first
 
 let isPlaying = false;
-
+console.log(isPlaying);
 
 // Generate the grid on click of "Play" button
 
@@ -34,7 +38,7 @@ playBtnElem.addEventListener("click", function () {
   if (isPlaying === false) {
 
     isPlaying = true;
-
+    console.log(isPlaying);
     // Generate grid container
     headerElem.insertAdjacentHTML("afterend", containerMarkup)
 
@@ -50,14 +54,18 @@ playBtnElem.addEventListener("click", function () {
 
 });
 
+// Remove grid and let the player to restart the game on click of "Reset" button
+
+resetBtnElem.addEventListener("click", resetGame);
+
 
 // #region ||||| FUNCTIONS |||||
 
 /**
  * Set the container with the given ID dimension based on the option chosen in the input with the given ID
  * 
- * @param {string} htmlInputID HTML ID of the input to get data from
- * @param {string} htmlContainerID HTML ID of the container to change the size of
+ * @param {string} htmlInputID HTML id of the input to get data from
+ * @param {string} htmlContainerID HTML id of the container to change the size of
  * 
  */
 function selectDifficulty(htmlInputID, htmlContainerID) {
@@ -68,7 +76,9 @@ function selectDifficulty(htmlInputID, htmlContainerID) {
 
   const containerElem = document.getElementById(htmlContainerID);
 
-  if (gameDifficulty === "medium") {
+  if (gameDifficulty === "easy") {
+    cellMarkup = `<div class="cell"></div>`;
+  } else if (gameDifficulty === "medium") {
     cellsNumber = 81;
     cellMarkup = `<div class="cell medium"></div>`
     containerElem.style.width = "calc(85px * 9)"
@@ -84,8 +94,8 @@ function selectDifficulty(htmlInputID, htmlContainerID) {
 /**
  * Generate a grid of elements with the given class in the given container
  * 
- * @param {string} htmlContainerID HTML ID of the container in which to generate the grid
- * @param {string} cssClass CSS Class to give to grid elements
+ * @param {string} htmlContainerID HTML id of the container in which to generate the grid
+ * @param {string} cssClass CSS class to give to grid elements
  * 
  */
 function generateGrid(htmlContainerID, cssClass) {
@@ -127,6 +137,27 @@ function generateGridElems(cssClass) {
 
     });
   }
+
+}
+
+
+/**
+ * Remove the grid, remove the classes from the elements and let the player to restart game
+ */
+function resetGame(htmlContainerID, cssClass) {
+
+  if (isPlaying === true) {
+
+    const containerElem = document.getElementById("game_container");
+
+    containerElem.remove();
+
+    isPlaying = false;
+    console.log(isPlaying);
+
+  }
+
+  cellsNumber = 100;
 
 }
 
