3665264373c2737e7d16274f37c8277349f32400
marinodilauro <m.dilauro90@gmail.com>
2024-02-28 14:20:03 +0100

    Add game container in HTML. ADD JS initial variables

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+// Create a variable for the "Play" button DOM element
+
+const playBtnElem = document.getElementById("play_btn");
+console.log(playBtnElem);
+
+// Create a variable for the header DOM element
+
+const headerElem = document.getElementById("site_header");
+console.log(headerElem);
+
+
+// Create a variable for the grid DOM element
+
+const gameContainerElem = document.getElementById("game_container");
+console.log(gameContainerElem);
+
+
+// Create a variable for the single grid element markup
+
+const cellMarkup = `<div class="cell"></div>`;
+console.log(cellMarkup);
