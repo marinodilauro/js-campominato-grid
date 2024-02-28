4e01eefce0f2d6bd1f0fa3aab620fb3648bac373
marinodilauro <m.dilauro90@gmail.com>
2024-02-28 14:35:37 +0100

    Add variable for cells number. Generate grid on click of play button

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,26 @@ const gameContainerElem = document.getElementById("game_container");
 console.log(gameContainerElem);
 
 
+// Create a variable for the number of the cells to generate
+
+const cellsNumber = 100;
+
+
 // Create a variable for the single grid element markup
 
 const cellMarkup = `<div class="cell"></div>`;
 console.log(cellMarkup);
+
+
+// Generate the grid on click of "Play" button
+
+playBtnElem.addEventListener("click", function () {
+
+  for (let i = 0; i < cellsNumber; i++) {
+
+    const cell = cellsNumber[i];
+    gameContainerElem.insertAdjacentHTML("afterbegin", cellMarkup)
+
+  }
+
+});
