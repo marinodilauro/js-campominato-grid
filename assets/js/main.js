8fcae4947612d7f7c157553d82916bf67e912907
marinodilauro <m.dilauro90@gmail.com>
2024-02-28 16:09:45 +0100

    Add cells number in console on click. Comment out debugging log

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,18 @@
 // Create a variable for the "Play" button DOM element
 
 const playBtnElem = document.getElementById("play_btn");
-console.log(playBtnElem);
+// console.log(playBtnElem);
 
 // Create a variable for the header DOM element
 
 const headerElem = document.getElementById("site_header");
-console.log(headerElem);
+// console.log(headerElem);
 
 
 // Create a variable for the grid DOM element
 
 const gameContainerMarkup = `<div id="game_container" class="d-flex flex-wrap"></div>`;
-console.log(gameContainerMarkup);
+// console.log(gameContainerMarkup);
 
 
 // Create a variable for the number of the cells to generate
@@ -23,13 +23,13 @@ const cellsNumber = 100;
 // Create a variable for the single grid element markup
 
 const cellMarkup = `<div class="cell"></div>`;
-console.log(cellMarkup);
+// console.log(cellMarkup);
 
 
 // Create a variable to prevent generation of multiple grids after the first
 
 let isPlaying = false;
-console.log(isPlaying);
+console.log("Is playing: " + isPlaying);
 
 
 // Generate the grid on click of "Play" button
@@ -53,7 +53,7 @@ playBtnElem.addEventListener("click", function () {
     };
   };
 
-  console.log(isPlaying);
+  console.log("Is playing: " + isPlaying);
 
   const cellElems = document.getElementsByClassName("cell");
 
@@ -67,7 +67,7 @@ playBtnElem.addEventListener("click", function () {
     cell.addEventListener("click", function () {
 
       cell.classList.toggle("clicked");
-      console.log();
+      console.log(cell.innerHTML);
 
     });
   }
